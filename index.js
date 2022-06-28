1263b7f3f714a81f672d932822f5f40b6d17ea93
Tulani <thulani@appimate.com>
2022-06-28 19:31:56 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)), 
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l= "+ l +"b=" + b );
+
+    if (l<=0 || b<= 0) {
+        console.log("L or B can not be 0")
+    }else{
+        console.log("Area = " + rect.area(l,b))
+        console.log("Perimeter = " + rect.perimeter(l,b))
+
+    }
+}
+
+solveRect(4,4);
+solveRect(0,4);
+solveRect(-3,4);
