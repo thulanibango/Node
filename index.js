8ba158edaa457bcfcb3c5f11844cd92be4f13925
Tulani <thulani@appimate.com>
2022-06-29 14:55:45 +0200

    callback and error handling

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ var rect = require('./rectangle');
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l= "+ l +"b=" + b );
+    rect(l,b ,(err, rectangle) => {
+        if(err){
+            console.log("Error ", err.message)
+        }else{
+            console.log("the area for the recatngle = " + rectangle.area());
+            console.log("the parameter for the recatngle = " + rectangle.perimeter())
+        }
 
-    if (l<=0 || b<= 0) {
-        console.log("L or B can not be 0")
-    }else{
-        console.log("Area = " + rect.area(l,b))
-        console.log("Perimeter = " + rect.perimeter(l,b))
+    })
+    console.log("Statement is after the call rect()")
 
-    }
+
 }
 
 solveRect(4,4);
--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,19 @@
+module.exports = (x,y, callback)=> {
+    if(x<=0 || y<= 0) {
+        setTimeout(()=>callback(
+            callback(new Error("L or B can not be 0"), null)
+        ), 5000);
+       
+    }else{
+        setTimeout(()=>
+            callback(null, 
+                {perimeter: () => (2*(x+y)),
+                    area: () => (x*y)}
+                ),
+                5000);
+    }
+}
 
-    exports.perimeter= (x,y) => (2*(x+y)), 
-    exports.area= (x,y) => (x*y)
+
+
+
